ff73bb5b7524f2489fb79241d40b2e3ed97ba752
data-pup <kevinmartinucr@gmail.com>
2018-05-28 15:16:22 -0400

    Added initial scaffold of a Rust implementation of the srcgen.py file. There are some details left to iron out, but this contains drafts most of the classes and methods that are found in the original srcgen file.

---
+++ lib/codegen/src/srcgen.rs
@@ -0,0 +1,203 @@
+use std::io;
+
+static SHIFTWIDTH: usize = 4;
+
+struct IndentedScope;
+impl IndentedScope {}
+
+struct Formatter {
+    indent: usize,
+    lines: Vec<String>
+}
+
+impl Formatter {
+    /// Source code formatter class.
+    ///
+    /// - Collect source code to be written to a file.
+    /// - Keep track of indentation.
+    ///
+    /// Indentation example: FIXUP: Convert example to Rust.
+    ///
+    ///    >>> f = Formatter()
+    ///    >>> f.line('Hello line 1')
+    ///    >>> f.writelines()
+    ///    Hello line 1
+    ///    >>> f.indent_push()
+    ///    >>> f.comment('Nested comment')
+    ///    >>> f.indent_pop()
+    ///    >>> f.format('Back {} again', 'home')
+    ///    >>> f.writelines()
+    ///    Hello line 1
+    ///        // Nested comment
+    ///    Back home again
+    pub fn new() -> Formatter {
+        Formatter {
+            indent: 0,
+            lines: Vec::new(),
+        }
+    }
+
+    /// Increase current indentation level by one.
+    pub fn indent_push(&mut self) {
+        // self.indent = format!("{}{:-2$}", self.indent, " ", SHIFTWIDTH);
+        self.indent += 1;
+    }
+
+    /// Decrease indentation by one level.
+    pub fn indent_pop(&mut self) {
+        assert!(self.indent > 0, "Already at top level indentation");
+        self.indent -= 1;
+    }
+
+    /// Get the current whitespace indentation in the form of a String.
+    fn get_indent(&self) -> String {
+        format!("{}{:-1$}", " ", self.indent * SHIFTWIDTH)
+    }
+
+    /// Get a string containing whitespace outdented one level. Used for
+    /// lines of code that are inside a single indented block.
+    fn get_outdent(&self) -> String {
+        let outdent_level = self.indent + 1;
+        format!("{}{:-1$}", " ", outdent_level * SHIFTWIDTH)
+    }
+
+    /// Add an indented line.
+    pub fn line(&mut self, contents: Option<&str>) {
+        let new_line = match contents {
+            Some(s) => format!("{}{}\n", self.get_indent(), s),
+            None => format!("{}\n", self.get_indent()),
+        };
+        self.lines.push(new_line);
+    }
+
+    /// Emit a line outdented one level.
+    pub fn outdented_line(&mut self, s: String) {
+        let new_line = format!("{}{}", self.get_outdent(), s);
+        self.lines.push(new_line);
+    }
+
+    /// Write all lines to `out`, or stdout if `out` is `None`.
+    pub fn writelines(&self, out: Option<&io::Write>) -> Result<(), io::Error> {
+        match out {
+            Some(w) => {
+                unimplemented!();
+                // FIXUP:
+                // self.lines.iter().map(|line| w.write(line))
+            },
+            None => {
+                self.lines.iter().for_each(|line| println!("{}", line));
+                Ok(())
+            },
+        }
+    }
+
+    /// Write `self.lines` to a file.
+    pub fn update_file(&self, filename: &str, directory: Option<&str>) {
+        unimplemented!();
+    }
+
+    /// FIXUP: Convert example code into Rust.
+    ///
+    /// Return a scope object for use with a `with` statement:
+    ///
+    ///    >>> f = Formatter()
+    ///    >>> with f.indented('prefix {', '} suffix'):
+    ///    ...     f.line('hello')
+    ///    >>> f.writelines()
+    ///    prefix {
+    ///        hello
+    ///    } suffix
+    ///
+    /// The optional `before` and `after` parameters are surrounding lines
+    /// which are *not* indented.
+    pub fn indented(&self, before: Option<&str>, after: Option<&str>) -> IndentedScope {
+        unimplemented!();
+    }
+
+    // TODO: Should this class implement a format trait?
+
+    /// Add one or more lines after stripping common indentation.
+    pub fn multi_line(&mut self, s: &str) {
+        parse_multiline(s).into_iter().for_each(|l| {
+            match l {
+                Some(s) => self.line(Some(s.as_str())),
+                None => self.line(None),
+            }
+        })
+    }
+
+    /// Add a comment line.
+    pub fn comment(&mut self, s: &str) {
+        let commented_line = format!("// {}", s);
+        self.line(Some(&commented_line));
+    }
+
+    /// Add a (multi-line) documentation comment.
+    pub fn doc_comment(&mut self, contents: &str) {
+        for line_contents in parse_multiline(contents) {
+            match line_contents {
+                Some(s) => {
+                    let commented_line = format!("/// {}", s);
+                    self.line(Some(commented_line.as_str()));
+                },
+                None => self.line(Some("///")),
+            };
+        }
+    }
+
+    /// FIXUP: Convert example code into Rust.
+    ///
+    /// Add a match expression.
+    ///
+    /// Example:
+    ///
+    ///    >>> f = Formatter()
+    ///    >>> m = Match('x')
+    ///    >>> m.arm('Orange', ['a', 'b'], 'some body')
+    ///    >>> m.arm('Yellow', ['a', 'b'], 'some body')
+    ///    >>> m.arm('Green', ['a', 'b'], 'different body')
+    ///    >>> m.arm('Blue', ['x', 'y'], 'some body')
+    ///    >>> f.match(m)
+    ///    >>> f.writelines()
+    ///    match x {
+    ///        Orange { a, b } |
+    ///        Yellow { a, b } => {
+    ///            some body
+    ///        }
+    ///        Green { a, b } => {
+    ///            different body
+    ///        }
+    ///        Blue { x, y } => {
+    ///            some body
+    ///        }
+    ///    }
+    pub fn add_match(&mut self, m: Match) {
+        unimplemented!();
+    }
+}
+
+/// Compute the indentation of s, or None of an empty line.
+fn indent(s: &str) -> Option<usize> {
+    if s.is_empty() {
+        None
+    } else {
+        let t = s.trim_left();
+        Some(s.len() - t.len())
+    }
+}
+
+fn parse_multiline(s: &str) -> Vec<Option<String>> {
+    unimplemented!();
+}
+
+struct Match {
+    expr: String,
+    // FIXUP: This will be a dictionary of some sort. Figure out the key/values.
+    // arms:
+}
+
+impl Match {
+    fn arm(&mut self, name: &str, fields: &[&str], body: &str) {
+        unimplemented!();
+    }
+}
