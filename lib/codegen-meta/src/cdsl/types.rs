88e8cc8cc6a238e2ae991552e4dbc8fe3965d1fb
data-pup <kevinmartinucr@gmail.com>
2018-06-13 16:51:57 -0400

    Added docstrings to the types.rs file.

--- lib/codegen-meta/src/cdsl/types.rs
+++ lib/codegen-meta/src/cdsl/types.rs
@@ -11,6 +11,7 @@
 // in the high 4 bits, giving a range of 2-256 lanes.
 static LANE_BASE: u16 = 0x70;
 
+// DEVELOPMENT NOTE: These were the original inheritance patterns in `types.py`
 // class ValueType(object):
 // class SpecialType(ValueType):
 // class LaneType(ValueType):
@@ -21,24 +22,50 @@ static LANE_BASE: u16 = 0x70;
 // class FlagsType(SpecialType):
 // class BVType(ValueType):
 
+// ValueType instances (i8, i32, ...) are provided in the `base.types` module.
+
+/// A concrete SSA value type.
+///
+/// All SSA values have a type that is described by an instance of `ValueType`
+/// or one of its subclasses.
 trait ValueType {}
+
+/// A concrete scalar type that can appear as a vector lane too.
+///
+/// Also tracks a unique set of :py:class:`VectorType` instances with this type
+/// as the lane type.
 trait LaneType {}
+
+/// A concrete scalar type that is neither a vector nor a lane type.
+///
+/// Special types cannot be used to form vectors.
 trait SpecialType {}
 
+/// A concrete SIMD vector type.
+///
+/// A vector type has a lane type which is an instance of :class:`LaneType`,
+/// and a positive number of lanes.
 struct _VectorType {}
 impl ValueType for _VectorType {}
 
+/// A concrete scalar integer type.
 struct _IntType {}
 impl LaneType for _IntType {}
 
+/// A concrete scalar floating point type.
 struct _FloatType {}
 impl LaneType for _FloatType {}
 
+/// A concrete scalar boolean type.
 struct _BoolType {}
 impl LaneType for _BoolType {}
 
+/// A type representing CPU flags.
+///
+/// Flags can't be stored in memory.
 struct _FlagsType {}
 impl SpecialType for _FlagsType {}
 
+/// A flat bitvector type. Used for semantics description only.
 struct _BVType {}
 impl ValueType for _BVType {}
