00bb4e8a0bc7a7b4e25b255a4bffc0e8b2fafed3
data-pup <kevinmartinucr@gmail.com>
2018-06-15 13:32:08 -0400

    Extremely broke commit, working on adding/defining the base types.

---
+++ lib/codegen-meta/src/base/types.rs
@@ -0,0 +1,36 @@
+//! This module predefines all the Cretonne scalar types.
+
+use cdsl::types as cdsl_types;
+
+// DEVELOPMENT NOTE: Original boolean type definition in Python.
+// #: Boolean.
+// b1 = BoolType(1)    #: 1-bit bool. Type is abstract (can't be stored in mem)
+// b8 = BoolType(8)    #: 8-bit bool.
+// b16 = BoolType(16)  #: 16-bit bool.
+// b32 = BoolType(32)  #: 32-bit bool.
+// b64 = BoolType(64)  #: 64-bit bool.
+
+// i8 = IntType(8)     #: 8-bit int.
+// i16 = IntType(16)   #: 16-bit int.
+// i32 = IntType(32)   #: 32-bit int.
+// i64 = IntType(64)   #: 64-bit int.
+
+/// 1-bit boolean.
+static b1: cdsl_types::BoolType = unimplemented!();
+/// 8-bit boolean.
+static b8: cdsl_types::BoolType = unimplemented!();
+/// 16-bit boolean.
+static b16: cdsl_types::BoolType = unimplemented!();
+/// 32-bit boolean.
+static b32: cdsl_types::BoolType = unimplemented!();
+/// 64-bit boolean.
+static b64: cdsl_types::BoolType = unimplemented!();
+
+/// 8-bit integer.
+static int8: cdsl_types::IntType = unimplemented!();
+/// 16-bit integer.
+static int16: cdsl_types::IntType = unimplemented!();
+/// 32-bit integer.
+static int32: cdsl_types::IntType = unimplemented!();
+/// 64-bit integer.
+static int64: cdsl_types::IntType = unimplemented!();
